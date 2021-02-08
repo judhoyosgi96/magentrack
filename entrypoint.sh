a1e98e9ff3671f5361b044450a444f14a82fb684
DESKTOP-AGRBT6B\j_u_a <juan.hoyos@bankami.co>
2021-02-08 18:25:06 -0500

    code path

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-python manage.py makemigrations
-python manage.py migrate
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | python manage.py shell
+python /code/manage.py makemigrations
+python /code/manage.py migrate
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', '', 'admin')" | python /code/manage.py shell
 
-echo "from apps.variable.models import Variable; Variable.objects.update_or_create(id=0,name='items_per_page',value='5')" | python manage.py shell
+echo "from apps.variable.models import Variable; Variable.objects.update_or_create(id=0,name='items_per_page',value='5')" | python /code/manage.py shell
 
-python manage.py runserver 0.0.0.0:8000
+python /code/manage.py runserver 0.0.0.0:8000
